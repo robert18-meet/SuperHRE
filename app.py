9f12e75d896c4b83d8b700e29fff1b1711d51517
robert18-meet <amiiir14.2@gmail.com>
2018-01-18 20:00:26 +0200

    fd

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from FlaskRouter import *
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
+from DB import *
+app = Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def homepage():
@@ -29,3 +34,7 @@ def Minigame():
 @app.route('/Songs')
 def Songs():
 	return render_template('Songs.html')  
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
